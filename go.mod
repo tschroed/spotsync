169d34df4ad00d372f145eb5fba188f6a264b642
Trevor Schroeder <tschroed@gmail.com>
2024-07-07 01:18:58 -0400

    Switch to scoped auth codes so we can...drumroll...modify the user library.

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/tschroed/spotsync
+module github.com/tschroed/spotsync/v2
 
-go 1.22
+go 1.22.5
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/zmb3/spotify v1.3.0 // indirect
+	github.com/tschroed/spotify/v2 v2.0.0-20240707013608-db8ad82919b6 // indirect
 	github.com/zmb3/spotify/v2 v2.4.2 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
--- search.go
+++ search.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
@@ -12,8 +13,6 @@ import (
 	"os"
 	"strings"
 
-	"golang.org/x/oauth2/clientcredentials"
-
 	"github.com/zmb3/spotify/v2"
 )
 
@@ -21,10 +20,19 @@ type roundtripLogger struct {
 	Transport http.RoundTripper
 }
 
-var dFlag = flag.Bool("d", false, "Enable debugging")
+var (
+	dFlag = flag.Bool("d", false, "Enable debugging")
+	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserLibraryModify))
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
 
 func (r roundtripLogger) RoundTrip(request *http.Request) (*http.Response, error) {
-	fmt.Println("request", request.URL.String())
+	fmt.Println("request:", request.Method, request.URL.String())
+	fmt.Println("headers:")
+	for k, v := range request.Header {
+		fmt.Printf("  %s: %s", k, v)
+	}
 	res, err := r.Transport.RoundTrip(request)
 	var bufReader bytes.Buffer
 	io.Copy(&bufReader, res.Body)
@@ -34,6 +42,32 @@ func (r roundtripLogger) RoundTrip(request *http.Request) (*http.Response, error
 	return res, err
 }
 
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://192.168.1.101:8080/callback"
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	httpClient := spotifyauth.New().Client(r.Context(), tok)
+	if (*dFlag) {
+		httpClient.Transport = roundtripLogger{Transport: httpClient.Transport}
+	}
+	client := spotify.New(httpClient)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+}
+
 func main() {
 	flag.Parse()
 	text := strings.Join(os.Args[1:], " ")
@@ -41,21 +75,31 @@ func main() {
 		log.Fatal("Please supply search terms on the command line")
 	}
 	ctx := context.Background()
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("SPOTIFY_ID"),
-		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
-		TokenURL:     spotifyauth.TokenURL,
-	}
-	token, err := config.Token(ctx)
-	if err != nil {
-		log.Fatalf("couldn't get token: %v", err)
-	}
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	httpClient := spotifyauth.New().Client(ctx, token)
-	if (*dFlag) {
-		httpClient.Transport = roundtripLogger{Transport: httpClient.Transport}
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
 	}
-	client := spotify.New(httpClient)
+	fmt.Println("You are logged in as:", user.ID)
+
 	results, err := client.Search(ctx, text, spotify.SearchTypeArtist|spotify.SearchTypeAlbum)
 	if err != nil {
 		log.Fatal(err)
@@ -63,7 +107,9 @@ func main() {
 
 	// handle album results
 	if results.Albums != nil {
+	    reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Albums:")
+		toAdd := make([]spotify.SimpleAlbum, 0)
 		for _, item := range results.Albums.Albums {
 			fmt.Println("   ", item.Name)
 			fmt.Println("    >> Artists:")
@@ -77,6 +123,24 @@ func main() {
 			fmt.Println("    >> Tracks:")
 			for _, track := range fa.Tracks.Tracks { // Assume just 1 page
 				fmt.Println("        ", track.Name)
+			}
+			fmt.Print("Add to library? [y/N] => ")
+			r, _ := reader.ReadString('\n')
+			r = strings.TrimSpace(r)
+			if r == "y" || r == "Y" {
+			  toAdd = append(toAdd, item)
+			}
+		}
+		if len(toAdd) > 0 {
+			fmt.Println("Adding...")
+			ids := make([]spotify.ID, len(toAdd))
+			for i, alb := range toAdd {
+				fmt.Println("    ", alb.Artists[0].Name, " / ", alb.Name)
+				ids[i] = alb.ID
+			}
+			err = client.AddAlbumsToLibrary(ctx, ids...)
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
 	}
